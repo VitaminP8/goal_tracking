ed7afd1c05f29792f549a1fb2640219a272c43c2
Alexey <alexey.zhukovich@gmail.com>
2023-05-26 14:25:08 +0300

    add goal form

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -37,3 +37,37 @@ class GoalForm(forms.ModelForm):
         # fields = ('name', 'foods')
         exclude = ('is_active', 'started_at', 'lateness', 'user')
 
+
+class GoalUpdateForm(forms.ModelForm):
+
+    name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'placeholder': "тема",
+            'class': 'form-control'
+        }
+    ))
+
+    description = forms.CharField(required=False, widget=forms.TextInput(
+        attrs={
+            'placeholder': 'описание',
+            'class': 'form-control'
+        }
+    ))
+
+    deadline = forms.DateField(widget=forms.SelectDateWidget(
+        attrs={
+            'placeholder': 'дедлайн',
+            'class': 'special'
+        }
+    ))
+
+    categories = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), widget=forms.CheckboxSelectMultiple(
+        attrs={'class': "special"}
+    ))
+
+    is_active = forms.BooleanField(required=False)
+    class Meta:
+        model = Goal
+        # fields = '__all__'
+        # fields = ('name', 'foods')
+        exclude = ('started_at', 'lateness', 'user')
